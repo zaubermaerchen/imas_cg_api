304f8d744fc36e116d05da7babe6cb386493a885
Mutsuki Kimuraya <sondessein@gmail.com>
2015-01-10 12:18:43 +0900

    MySQL-PythonがインストールされていなかったらPyMySQLを使用するように

--- api/wsgi.py
+++ api/wsgi.py
@@ -14,12 +14,18 @@ framework.
 
 """
 import os
+import api.module
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "api.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
+
+# If MySQL-python is not installed then use PyMySQL
+if "MySQLdb" not in api.module.names:
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
--- manage.py
+++ manage.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import os
 import sys
+import api.module
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
 
-    import pymysql
-    pymysql.install_as_MySQLdb()
-    
+    # If MySQL-python is not installed then use PyMySQL
+    if "MySQLdb" not in api.module.names:
+        import pymysql
+        pymysql.install_as_MySQLdb()
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
