38051ce82732cd76adf0fbd0c7476ac06125fc2a
Mutsuki Kimuraya <sondessein@gmail.com>
2018-12-16 17:49:50 +0900

    完全移行するまで暫定に追加

--- api/cartoon/serializer.py
+++ api/cartoon/serializer.py
@@ -5,6 +5,7 @@ from data.models import Cartoon
 
 class SearchSerializer(serializers.ModelSerializer):
     characters = serializers.SerializerMethodField(read_only=True)
+    idols = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = Cartoon
@@ -13,12 +14,17 @@ class SearchSerializer(serializers.ModelSerializer):
             'title',
             'date',
             'characters',
+            'idols',
             'comment',
             'thumbnail_hash',
         ]
 
     @staticmethod
     def get_characters(obj):
+        return obj.idols.split()
+
+    @staticmethod
+    def get_idols(obj):
         return obj.idols.split()
 
 
