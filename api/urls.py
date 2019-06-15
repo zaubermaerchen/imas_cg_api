9acbfff3f374ed2f7a4caad1102e0532dd69e4ab
Mutsuki Kimuraya <sondessein@gmail.com>
2019-06-15 12:03:37 +0900

    パス変更

--- api/urls.py
+++ api/urls.py
@@ -1,21 +1,10 @@
-from django.conf.urls import include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
-admin.autodiscover()
+from django.urls import path, include
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'api.views.home', name='home'),
-    # url(r'^api/', include('api.foo.urls')),
-    url(r'^imas_cg/api/idol/', include('api.idol.urls')),
-    url(r'^imas_cg/api/skill/', include('api.skill.urls')),
-    url(r'^imas_cg/api/idol_name/', include('api.idol_name.urls')),
-    url(r'^imas_cg/api/cartoon/', include('api.cartoon.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^imas_cg/api/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('idol/', include('api.idol.urls')),
+    path('skill/', include('api.skill.urls')),
+    path('idol_name/', include('api.idol_name.urls')),
+    path('cartoon/', include('api.cartoon.urls')),
 ]
