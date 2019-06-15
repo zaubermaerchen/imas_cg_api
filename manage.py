416718af17c071b5db89e783ae7dde44563dc544
Mutsuki Kimuraya <sondessein@gmail.com>
2019-06-15 11:58:28 +0900

    PyMySQLからmysqlclientに変更

--- manage.py
+++ manage.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
-import api.module
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings")
-
-    # If MySQL-python is not installed then use PyMySQL
-    if "MySQLdb" not in api.module.names:
-        import pymysql
-        pymysql.install_as_MySQLdb()
 
     from django.core.management import execute_from_command_line
 
