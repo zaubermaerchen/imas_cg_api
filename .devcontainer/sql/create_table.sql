391d44149e826e3fe6c9b1d1f031d65ba17c1c90
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2026-02-21 22:37:44 +0900

    devcontainer設定追加

---
+++ .devcontainer/sql/create_table.sql
@@ -0,0 +1,69 @@
+DROP TABLE IF EXISTS `idol`;
+CREATE TABLE `idol` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `name` varchar(256) NOT NULL DEFAULT '',
+  `type` int(11) NOT NULL DEFAULT '0',
+  `rarity` int(11) NOT NULL DEFAULT '0',
+  `cost` int(11) NOT NULL DEFAULT '0',
+  `offense` int(11) NOT NULL DEFAULT '0',
+  `defense` int(11) NOT NULL DEFAULT '0',
+  `max_offense` int(11) NOT NULL DEFAULT '0',
+  `max_defense` int(11) NOT NULL DEFAULT '0',
+  `skill_name` varchar(256) NOT NULL DEFAULT '',
+  `skill_id` int(11) NOT NULL DEFAULT '0',
+  `skill2_id` int(11) NOT NULL DEFAULT '0',
+  `hash` char(32) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FULLTEXT INDEX `name_index` (`name`),
+  KEY `idol_idx1` (`type`,`rarity`)
+) ENGINE=Mroonga DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `skill`;
+CREATE TABLE `skill` (
+  `id` int(11) NOT NULL,
+  `target_unit` int(11) NOT NULL DEFAULT '0',
+  `target_member` int(11) NOT NULL DEFAULT '0',
+  `target_type` int(11) NOT NULL DEFAULT '0',
+  `target_num` int(11) NOT NULL DEFAULT '-1',
+  `target_param` int(11) NOT NULL DEFAULT '0',
+  `skill_value_id` int(11) NOT NULL DEFAULT '0',
+  `comment` varchar(256) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+DROP TABLE IF EXISTS `skill_value`;
+CREATE TABLE `skill_value` (
+  `id` int(11) NOT NULL,
+  `value1` double NOT NULL DEFAULT '0',
+  `value2` double NOT NULL DEFAULT '0',
+  `value3` double NOT NULL DEFAULT '0',
+  `value4` double NOT NULL DEFAULT '0',
+  `value5` double NOT NULL DEFAULT '0',
+  `value6` double NOT NULL DEFAULT '0',
+  `value7` double NOT NULL DEFAULT '0',
+  `value8` double NOT NULL DEFAULT '0',
+  `value9` double NOT NULL DEFAULT '0',
+  `value10` double NOT NULL DEFAULT '0',
+  `value11` double NOT NULL DEFAULT '0',
+  `value12` double NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+DROP TABLE IF EXISTS `cartoon`;
+DROP TABLE IF EXISTS `idol_name`;
+
+CREATE TABLE `idol_name` (
+  `name` varchar(255) PRIMARY KEY
+) ENGINE=Mroonga DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='default_tokenizer "TokenDelimit"';
+
+CREATE TABLE `cartoon` (
+  `id` int(11) NOT NULL DEFAULT '0' PRIMARY KEY,
+  `title` varchar(256) NOT NULL DEFAULT '',
+  `date` date NOT NULL DEFAULT '1970-01-01',
+  `idols` text COMMENT 'flags "COLUMN_VECTOR", type "idol_name"',
+  `comment` varchar(256) NOT NULL DEFAULT '',
+  `thumbnail_hash` char(32) DEFAULT NULL,
+  FULLTEXT INDEX `title_index` (`title`),
+  FULLTEXT INDEX `idols_index` (`idols`) COMMENT 'table "idol_name"'
+) ENGINE=Mroonga DEFAULT CHARSET=utf8mb4;
--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-poetry config virtualenvs.create false
-poetry install --no-root
-
-exec "$@"
