1c0580391f22568938a49c8afbfd3bb9eb297033
zaubermaerchen <9396256+zaubermaerchen@users.noreply.github.com>
2026-02-22 16:46:59 +0900

    使用されていないアイドル一覧リストAPIを削除

--- api/idol/urls.py
+++ api/idol/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from .views import GetView, SearchView
-from api.idol import views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,5 +8,4 @@ from api.idol import views
 urlpatterns = [
     path('<int:pk>/', GetView.as_view()),
     path('search/', SearchView.as_view()),
-    path('list/', views.get_list),
 ]
